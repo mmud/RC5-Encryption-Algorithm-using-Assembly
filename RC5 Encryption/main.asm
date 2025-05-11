08139e6a52aafda950a5a998624cd263d47dbf1d
mmud <m7mud.xd.gamer2@gmail.com>
2025-05-11 18:58:37 +0300

    v0.0.2

--- RC5 Encryption/main.asm
+++ RC5 Encryption/main.asm
@@ -3,355 +3,258 @@
 
 .include "m328Pdef.inc"
 
-; Define Variables:
-.EQU PL = 0xe1 
-.EQU PH = 0xb7
-.EQU QL = 0x37
-.EQU QH = 0x9e
-.EQU R = 8    
-.EQU T = 18   
-.EQU W = 16   
-.EQU U = 2    
-.EQU B = 12   
-.EQU C = 6    
-.EQU N = 54   
-; Memory locations for K[i]
-.EQU BY0 = 0x0200
-.EQU BY1 = 0x0201
-.EQU BY2 = 0x0202
-.EQU BY3 = 0x0203
-.EQU BY4 = 0x0204
-.EQU BY5 = 0x0205
-.EQU BY6 = 0x0206
-.EQU BY7 = 0x0207
-.EQU BY8 = 0x0208
-.EQU BY9 = 0x0209
-.EQU BY10 = 0x020A
-.EQU BY11 = 0x020B
+.EQU ROUNDS =  8
+.EQU DATA_SIZE = 0x01
+.EQU DATA_IN = 0x0280
+.EQU ENC_DATA = 0x0400
+.EQU DEC_DATA = 0x0500
+.EQU Ko = 0x0200
+.EQU So = 0x0210
 
-; MACROs
-.MACRO INPUT
-    .DEF AH = R17
-    ldi AH, high(@0)   
-    .DEF AL = R16
-    ldi AL, low(@0)    
-    .DEF BH = R19
-    ldi BH, high(@1)   
-    .DEF BL = R18
-    ldi BL, low(@1)    
-.ENDMACRO
-.MACRO SECRET_KEY
-        LDI R20, @11
-        STS BY0, R20
-        LDI R20, @10
-        STS BY1, R20
-        LDI R20, @9
-        STS BY2, R20
-        LDI R20, @8
-        STS BY3, R20
-        LDI R20, @7
-        STS BY4, R20
-        LDI R20, @6
-        STS BY5, R20
-        LDI R20, @5
-        STS BY6, R20
-        LDI R20, @4
-        STS BY7, R20
-        LDI R20, @3
-        STS BY8, R20
-        LDI R20, @2
-        STS BY9, R20
-        LDI R20, @1
-        STS BY10, R20
-        LDI R20, @0
-        STS BY11, R20
-.ENDMACRO
-.MACRO ROTL_WORD
-        TST @2
-        BREQ ZEROL
-        MOV R25, @2         
-    ROTL:
-        ROL @1
-        BST @0, 7
-        ROL @0
-        BLD @1, 0
-        DEC R25
-        BRNE ROTL
-    ZEROL:
-        nop
-.ENDMACRO
-.MACRO ROTR_WORD
-        TST @2
-        BREQ ZEROR
-        MOV R25, @2          
-    ROTR:
-        ROR @0
-        BST @1, 0
-        ROR @1
-        BLD @0, 7
-        DEC R25
-        BRNE ROTR
-    ZEROR:
-        nop
-.ENDMACRO
-.MACRO XOR_WORD            
-        EOR @1, @3
-        EOR @0, @2
-.ENDMACRO
-.MACRO SUB_WORD             
-        SUB @1, @3
-        SBC @0, @2
-.ENDMACRO
-.MACRO ADD_WORD            
-        ADD @1, @3
-        ADC @0, @2
-.ENDMACRO
+.macro ROTL 
+	ROTL_LOOP:
+		clc
+		SBRC @0 , 7
+		SEC
+		ROL @1
+		ROL @0
+		DEC @2
+		BRNE ROTL_LOOP
+.endmacro
 
-;==============================================================================
-;*RC5_SETUP**********************************
-;==============================================================================
+.macro ROTR
+	ROTR_LOOP:
+		clc
+		SBRC @1 , 0
+		SEC
+		ROR @0
+		ROR @1
+		DEC @2
+		BRNE ROTR_LOOP
+.endmacro
 
-.MACRO RC5_SETUP
+.MACRO KEY_EXP
+	ldi Zl , low(So)  ; Loading address of So in Z
+	ldi Zh , high(So) 
+	ldi r16 , low(0xB7E1) ; loading Pw in r17 r16 
+	ldi r17 , high(0xB7E1)
+	st  Z  , r16    ; Storing Pw as So 
+	std  Z+1  , r17
+	ldi r18, low(0x9E37)	; Qw in Registers  r19 r18 
+	ldi r19, high(0x9E37)	
+	ldi r20 , 17        ; number of iterations
+	LOOP_K1 :
+		ld r16 , Z+ ; loading S[i-1] in r17  r16
+		ld r17 , Z+
+		add r16 , r18 ; adding S[i-1] to Qw
+		adc r17  , r19 
+		st z , r16 ; Storing in the next location
+		std z+1 , r17 
+		dec r20
+		brne LOOP_K1
+	ldi Yl , LOW(Ko) ; Loading address of Ko in Y
+	ldi Yh , HIGH(Ko)
+	ldi Zl , low(So)  ; Loading address of So in Z
+	ldi Zh , high(So)
+	clr R0 ; A in R0 and R1
+	clr R1
+	clr R2 ; B in R2 and R3
+	clr R3
+	ldi R25 , 54
+	LOOP_K2:
+		add R0 , R2 ; A = A + B
+		adc R1 , R3
+		ld R4 , Z   ; Loading Si to R4,R5
+		ldd R5 , Z+1
+		add R0 , R4 ; A = Si + A
+		adc R1 , R5
+		ldi R22 , 3 ; Rotation Counter
+		ROTL R0 , R1 , R22 ; Rotating A Left 3 times
+		st Z , R0 ; Storing A in S[i]
+		std Z+1 , R1 
+		add R2 , R0 ; B = B + A
+		adc R3 , R1
+		mov R22, R2 ; Setting Rotation Counter
+		andi R22,0x0F
+		ld R4 , Y ; Loading L[i]
+		ldd R5 , Y+1
+		add R2 , R4 ; B = L[i] + B
+		adc R3 , R5
+		ROTL R2,R3,R22
+		st Y , R2 ; Storing B in L[i]
+		std Y+1 , R3
+		;;;;; Checking if exceeding the S array (mod)
+		I_RESET
+		;;;;; Checking if exceeding the L array (mod)
+		J_RESET
+ 
+		dec R25
+		brne LOOP_K2
+.ENDMACRO	
 
-		;----------------------------------------------------------------
-        ; ----- Convert secret key (K[i]) to 6 words (L[i]) -------------
-		;----------------------------------------------------------------
+.MACRO ENCRYPTION
+	ldi yl , LOW(DATA_IN) ; Pointer on the start of input data
+	ldi yh , HIGH(DATA_IN) 
+	ldi xl , LOW(ENC_DATA) ; Pointer on the Envrypted Data location
+	ldi xh , HIGH(ENC_DATA)
+ 	ldi R25 , DATA_SIZE 
+DATA_ENCRY:
+	ldi zl , LOW(So) ; Pointer on So
+	ldi zh , HIGH(So)
+	ld R16 , y+ ; Loading A in R16, R17
+	ld R17 , y+ 
+	ld R18 , y+ ; Loading B in R18 , R19
+	ld R19 , y+
+	ld R22 , z+ ; Loading S[0]
+	ld R23 , z+
+	add R16 , R22 ; A + S[0]
+	adc R17 , R23 
+	ld R22 , z+  ; Loading S[1]
+	ld R23 , z+
+	add R18 , R22 ; B + S[1]
+	adc R19 , R23
+	ldi R24 , ROUNDS ; Counter for encryption rounds
 
-       
-        ; Build L5:
-        lds  r0, BY11
-        lds  r1, BY10
-        sts  0x022A, r1     
-        sts  0x022B, r0 
-        ; Build L4:
-        lds  r0, BY9
-        lds  r1, BY8
-        sts  0x0228, r1
-        sts  0x0229, r0
-        ; Build L3:
-        lds  r0, BY7
-        lds  r1, BY6
-        sts  0x0226, r1
-        sts  0x0227, r0
-        ; Build L2:
-        lds  r0, BY5
-        lds  r1, BY4
-        sts  0x0224, r1
-        sts  0x0225, r0
-        ; Build L1:
-        lds  r0, BY3
-        lds  r1, BY2
-        sts  0x0222, r1
-        sts  0x0223, r0
-        ; Build L0:
-        lds  r0, BY1
-        lds  r1, BY0
-        sts  0x0220, r1
-        sts  0x0221, r0
-		;----------------------------------------------------
-        ; ----- Step 2: Initialize expanded key table S[i] -----
-		;----------------------------------------------------
-        .EQU S0L = 0x0210
-        .EQU S0H = 0x0211
-        .EQU S1L = 0x0212
-        .EQU S1H = 0x0213
-
-        ldi ZL, low(S0L)
-        ldi ZH, high(S0L)
-
-        ldi R21, PL
-        ldi R22, PH
-        STS S0L, R21
-        STS S0H, R22
-
-        ldi R21, QL
-        ldi R22, QH
-        ldi R20, T
-        subi R20, 1
-    LOOP_S:
-        LD R23, Z+          ; get low byte from current S[i]
-        LD R24, Z+          ; get high byte from current S[i]
-        ADD_WORD R24, R23, R22, R21   ; S[i] + Qw
-        ST Z, R23           ; store result low byte
-        STD Z+1, R24        ; store result high byte
-        DEC R20
-        BRNE LOOP_S
-		;------------------------------------------------------
-        ; ----- Step 3: Key Expansion (mixing in L array) -----
-		;------------------------------------------------------
-        clr R0
-        clr R1
-        clr R2
-        clr R3
-        ldi ZL, low(S0L)
-        ldi ZH, high(S0L)
-        ldi YL, low(0x0220)   
-        ldi YH, high(0x0220)
-        ldi R20, N
-    LOOP_MIX:
-        ADD_WORD R1, R0, R3, R2
-        LD R23, Z
-        LDD R24, Z+1
-        ADD_WORD R1, R0, R24, R23
-        ldi R22, 3
-        ROTL_WORD R1, R0, R22
-        ST Z, R0
-        STD Z+1, R1
-        ADD_WORD R3, R2, R1, R0
-        mov R22, R2
-        andi R22, 0x0F
-        LD R23, Y
-        LDD R24, Y+1
-        ADD_WORD R3, R2, R24, R23
-        ROTL_WORD R3, R2, R22
-        ST Y, R2
-        STD Y+1, R3
-        RCALL I_RESET
-        RCALL J_RESET
-        DEC R20
-        BRNE LOOP_MIX
+ENCRYPTION_ROUND : 
+	EOR R16 , R18 ; A XOR B
+	EOR R17 , R19
+	MOV R20 , R18 ; Taking the lowest 4 bits of B  as the counter
+	ANDI R20 , 0x0F
+	ROTL R16 , R17 , R20 
+	ld R22 , z+ ; Loading S[2i]
+	ld R23 , z+
+	add R16 ,R22 ; A = A + S[2i] 
+	adc R17 , R23
+	;;;
+	EOR R18 , R16 ; B XOR A
+	EOR R19 , R17
+	MOV R20 , R16 ; Taking the lowest 4 bits of A  as the counter
+	ANDI R20 , 0x0F
+	ROTL R18 , R19 , R20
+	ld R22 , z+ ; Loading S[2i + 1]
+	ld R23 , z+
+	add R18 , R22 ; B = B + S[2i + 1]
+	adc R19 , R23
+	dec R24 ; Decrementing the branch rounds counter
+	brne ENCRYPTION_ROUND
+	st  x+ , R16 
+	st  x+ , R17 
+	st  x+ , R18 
+	st  x+ , R19 
+	dec R25 ; Decrementing the input data size counter
+	brne DATA_ENCRY
 .ENDMACRO
 
-;==============================================================================
-;RC5_ENCRYPT*********************************
-;==============================================================================
+.MACRO DECRYPTION
+	ldi xl , LOW(ENC_DATA)
+	ldi xh , HIGH(ENC_DATA)
+	ldi yl , LOW(DEC_DATA) ; Pointer on the start of input data
+	ldi yh , HIGH(DEC_DATA) 
+ 	ldi R25 , DATA_SIZE 
 
-.MACRO RC5_ENCRYPT
-        LDI XL, 0x14
-        LDI XH, 0x02
-        LDS R22, S0L
-        LDS R21, S0H
-        ADD_WORD AH, AL, R21, R22
-        LDS R22, S1L
-        LDS R21, S1H
-        ADD_WORD BH, BL, R21, R22
-        LDI R20, R
-    LOOP_E:
-        LDI R22, 0x0F
-        AND R22, BL  ;%16
-        LD R23, X+
-        LD R24, X+
-        XOR_WORD AH, AL, BH, BL
-        ROTL_WORD AH, AL, R22
-        ADD_WORD AH, AL, R24, R23
-        LDI R22, 0x0F
-        AND R22, AL  ;%16
-        LD R23, X+
-        LD R24, X+
-        XOR_WORD BH, BL, AH, AL
-        ROTL_WORD BH, BL, R22
-        ADD_WORD BH, BL, R24, R23
-        DEC R20
-        BRNE LOOP_E
+DATA_DECRY:
+	ldi zl , LOW(0x234) ; Last location of S array
+	ldi zh , HIGH(0x234).
+	ldi R24 , ROUNDS ; Counter for Decryption rounds
+	ld R16 , x+ ;Loading A from Encrypted Data
+	ld R17 , x+
+	ld R18 , x+	 ;Loading B from Envrypted Data
+ 	ld R19 , x+
+DECRYPTION_ROUND : 
+	ld R23 , -z  ;Loading needed S 
+	ld R22 , -z 
+	sub R18, R22 ; B = B - S
+	sbc R19 , R23 
+	MOV R20, R16 ; Taking A lowest 4 bits as a rotation counter
+	ANDI R20 , 0x0F
+	ROTR R18 , R19 , R20
+	EOR R18 , R16 ; B XOR A
+	EOR R19 , R17
+	;;;
+	ld R23 , -z  ;Loading needed S 
+	ld R22 , -z 
+	sub R16  , R22 ; A = A - S
+	sbc R17 ,  R23
+	MOV R20, R18 ; Taking A lowest 4 bits as a rotation counter
+	ANDI R20 , 0x0F
+	ROTR R16 , R17 , R20
+	EOR R16 , R18 ; A XOR B
+	EOR R17 , R19
+	dec R24
+	brne DECRYPTION_ROUND 
+	ld R23 , -z  ;Loading needed S 
+	ld R22 , -z 
+	sub R18, R22 ; B = B - S
+	sbc R19 , R23 
+	ld R23 , -z  ;Loading needed S 
+	ld R22 , -z
+	sub R16  , R22 ; A = A - S
+	sbc R17 ,  R23
+	st  y+ , R16 
+	st  y+ , R17 
+	st  y+ , R18 
+	st  y+ , R19
+	dec R25
+	brne DATA_DECRY
+.ENDMACRO 
+
+.MACRO I_RESET
+    inc  zl          ; Increment low byte twice (16-bit values)
+    inc  zl
+    ldi  r20, 0x34   ; Last location of S array (0x234)
+    cpse zl, r20     ; Compare low bytes
+    rjmp i_reset_cont ; Jump to continuation if not at end
+    ldi  zl, LOW(So) ; Reset to start of S array (0x210)
+i_reset_cont:        ; Continue execution here
 .ENDMACRO
 
-;==============================================================================
-;*RC5_DECRYPT**********************************
-;==============================================================================
-
-.MACRO RC5_DECRYPT
-        LDI XL, 0x34
-        LDI XH, 0x02
-        LDI R20, 8
-    LOOP_D:
-        LDI R22, 0x0F
-        AND R22, AL
-        LD R23, -X
-        LD R24, -X
-        SUB_WORD BH, BL, R23, R24
-        ROTR_WORD BH, BL, R22
-        XOR_WORD BH, BL, AH, AL
-        LDI R22, 0x0F
-        AND R22, BL
-        LD R23, -X
-        LD R24, -X
-        SUB_WORD AH, AL, R23, R24
-        ROTR_WORD AH, AL, R22
-        XOR_WORD AH, AL, BH, BL
-        DEC R20
-        BRNE LOOP_D
-        LDS R22, S0H
-        LDS R21, S0L
-        SUB_WORD AH, AL, R22, R21
-        LDS R22, S1H
-        LDS R21, S1L
-        SUB_WORD BH, BL, R22, R21
+.MACRO J_RESET
+    inc  yl          ; Increment low byte twice (16-bit values)
+    inc  yl
+    ldi  r20, 0x0C   ; Last location of L array (0x20C)
+    cpse yl, r20     ; Compare low bytes
+    rjmp j_reset_cont ; Jump to continuation if not at end
+    clr  yl          ; Reset to start of L array (0x200)
+j_reset_cont:        ; Continue execution here
 .ENDMACRO
 
-
-
-;==============================================================================
-;*LCD_Writer***********************************
-;==============================================================================
-
-
-
-.MACRO LCD
-
-;================================================================
+.MACRO lcd
 LCD_write_2:
       LDI   R25, 0xFF
-      OUT   DDRD, R25         ;set port D o/p for data
-      OUT   DDRB, R25         ;set port B o/p for command
-      CBI   PORTB, 0          ;EN = 0
-      RCALL delay_ms_2        ;wait for LCD power on
-      ;-----------------------------------------------------
-      RCALL LCD_init_2        ;subroutine to initialize LCD
-      ;-----------------------------------------------------
-      RCALL disp_message_2    ;subroutine to display message (only once)
-      
-      ; End of program - infinite loop to stop execution
+      OUT   DDRD, R25         
+      OUT   DDRB, R25         
+      CBI   PORTB, 0         
+      RCALL delay_ms_2        
+      RCALL LCD_init_2     
+      RCALL disp_message_2    
 end_2:  RJMP  end_dis
-;================================================================
 LCD_init_2:
-      LDI   R25, 0x33         ;init LCD for 4-bit data
+      LDI   R25, 0x33         
       RCALL command_wrt_2       
       RCALL delay_ms_2
       LDI   R25, 0x32         
       RCALL command_wrt_2
       RCALL delay_ms_2
-      LDI   R25, 0x28         ;LCD 2 lines, 5x7 matrix
+      LDI   R25, 0x28        
       RCALL command_wrt_2
       RCALL delay_ms_2
-      LDI   R25, 0x0C         ;disp ON, cursor OFF
+      LDI   R25, 0x0C         
       RCALL command_wrt_2
-      LDI   R25, 0x01         ;clear LCD
+      LDI   R25, 0x01         
       RCALL command_wrt_2
       RCALL delay_ms_2
-      LDI   R25, 0x06         ;shift cursor right
+      LDI   R25, 0x06         
       RCALL command_wrt_2
       RET  
-;================================================================
 command_wrt_2:
-      MOV   R30, R25          ;use R30 instead of R27
-      ANDI  R30, 0xF0         ;mask low nibble & keep high nibble
-      OUT   PORTD, R30        ;o/p high nibble to port D
-      CBI   PORTB, 1          ;RS = 0 for command
-      SBI   PORTB, 0          ;EN = 1
-      RCALL delay_short_2       
-      CBI   PORTB, 0          ;EN = 0
-      RCALL delay_us_2          
-      ;----------------------------------------------------
       MOV   R30, R25          
-      SWAP  R30               ;swap nibbles
       ANDI  R30, 0xF0         
       OUT   PORTD, R30        
+      CBI   PORTB, 1          
       SBI   PORTB, 0          
       RCALL delay_short_2       
       CBI   PORTB, 0          
       RCALL delay_us_2          
-      RET
-;================================================================
-data_wrt_2:
-      MOV   R30, R25          ;use R30 instead of R27
-      ANDI  R30, 0xF0         
-      OUT   PORTD, R30        
-      SBI   PORTB, 1          ;RS = 1 for data
-      SBI   PORTB, 0          ;EN = 1
-      RCALL delay_short_2       
-      CBI   PORTB, 0          
-      RCALL delay_us_2          
-      ;----------------------------------------------------
       MOV   R30, R25          
       SWAP  R30               
       ANDI  R30, 0xF0         
@@ -361,50 +264,62 @@ data_wrt_2:
       CBI   PORTB, 0          
       RCALL delay_us_2          
       RET
-;================================================================
+data_wrt_2:
+      MOV   R30, R25          
+      ANDI  R30, 0xF0         
+      OUT   PORTD, R30        
+      SBI   PORTB, 1          
+      SBI   PORTB, 0         
+      RCALL delay_short_2       
+      CBI   PORTB, 0          
+      RCALL delay_us_2          
+      MOV   R30, R25          
+      SWAP  R30               
+      ANDI  R30, 0xF0         
+      OUT   PORTD, R30        
+      SBI   PORTB, 0          
+      RCALL delay_short_2       
+      CBI   PORTB, 0          
+      RCALL delay_us_2          
+      RET
 disp_message_2:
-      MOV   R25, R17          ;display characters via R25
-      RCALL data_wrt_2          
-      RCALL delay_seconds_2	  
-	  MOV   R25, R16          ;display characters via R25
-      RCALL data_wrt_2          
-      RCALL delay_seconds_2  
-	  MOV   R25, R19          ;display characters via R25
+      MOV   R25, R17          
       RCALL data_wrt_2          
       RCALL delay_seconds_2
-	  MOV   R25, R18          ;display characters via R25
+	  MOV   R25, R16          
+      RCALL data_wrt_2          
+      RCALL delay_seconds_2
+	  MOV   R25, R19         
+      RCALL data_wrt_2          
+      RCALL delay_seconds_2
+	  MOV   R25, R18         
       RCALL data_wrt_2          
       RCALL delay_seconds_2     
-      ;----------------
-      LDI   R28, 12           ;wait 3 seconds (using R28)
+      LDI   R28, 12           
 l2_2:   RCALL delay_seconds_2
       DEC   R28
       BRNE  l2_2
       RET
-;================================================================
 delay_short_2:
       NOP
       NOP
       RET
-;------------------------
 delay_us_2:
-      LDI   R20, 90           ;allowed register
+      LDI   R20, 90           
 l3_2:   RCALL delay_short_2
       DEC   R20
       BRNE  l3_2
       RET
-;-----------------------
 delay_ms_2:
-      LDI   R29, 40           ;using R29
+      LDI   R29, 40           
 l4_2:   RCALL delay_us_2
       DEC   R29
       BRNE  l4_2
       RET
-;================================================================
 delay_seconds_2:        
-    LDI   R20, 255            ;outer loop (allowed)
-l5_2: LDI   R29, 255            ;mid loop (R29)
-l6_2: LDI   R30, 20             ;inner loop (R30)
+    LDI   R20, 255           
+l5_2: LDI   R29, 255            
+l6_2: LDI   R30, 20            
 l7_2: DEC   R30         
     BRNE  l7_2          
     DEC   R29         
@@ -412,64 +327,56 @@ l7_2: DEC   R30
     DEC   R20         
     BRNE  l5_2          
     RET               
-;----------------------------------------------------------------
 end_dis:
 nop
-.ENDMACRO
+.ENDMACRO		
 
+Main:
+	; Secret Key "MahmoudShabn"
+	ldi r16,0x4D
+	sts 0x0200, r16
+	ldi r16,0x61
+	sts 0x0201, r16
+	ldi r16,0x68
+	sts 0x0202, r16
+	ldi r16,0x6D
+	sts 0x0203, r16
+	ldi r16,0x6F
+	sts 0x0204, r16
+	ldi r16,0x75
+	sts 0x0205, r16
+	ldi r16,0x64
+	sts 0x0206, r16
+	ldi r16,0x53
+	sts 0x0207, r16
+	ldi r16,0x68
+	sts 0x0208, r16
+	ldi r16,0x61
+	sts 0x0209, r16
+	ldi r16,0x62
+	sts 0x020A, r16
+	ldi r16,0x6E
+	sts 0x020B, r16
 
+	; Test Case "NDPR" (0x4E44 and 0x5052)
+	ldi r16,0x44  ; 'D'
+	sts 0x0280, r16
+	ldi r16,0x4E  ; 'N'
+	sts 0x0281, r16
+	ldi r16,0x52  ; 'R'
+	sts 0x0282, r16
+	ldi r16,0x50  ; 'P'
+	sts 0x0283, r16
 
-;=======================================================================
-;*TEST**********************************
-;=======================================================================
+	lds R16, 0x0280  ; Load 'D' (0x44)
+    lds R17, 0x0281  ; Load 'N' (0x4E)
+    lds R18, 0x0282  ; Load 'R' (0x52)
+    lds R19, 0x0283  ; Load 'P' (0x50)
 
-start:
-	ldi r20, high(RAMEND)
-	out SPH, r20
-	ldi r20, low(RAMEND)
-	out SPL, r20
-
-    SECRET_KEY 0x4D,0x61,0x68,0x6D,0x6F,0x75,0x64,0x53,0x68,0x61,0x62,0x6E
-    RC5_SETUP
-
-	; Test case: "NDPR" as 0x4E44 and 0x5052
-	INPUT 0x4E44, 0x5052
-	LCD
-
-	RC5_ENCRYPT
-	LCD
-
-	RC5_DECRYPT
-	LCD
-
-; Test case: "MASS" as 0x4D41 and 0x5353
-	INPUT 0x4D41, 0x5353
-	LCD
-
-	RC5_ENCRYPT
-	LCD
-
-	RC5_DECRYPT
-	LCD
-
-end_f: 
-	 RJMP  end_f
-
-	 I_RESET:
-        inc ZL
-        inc ZL
-        ldi R21, 0x34
-        cpse ZL, R21
-        ret    
-        ldi ZL, low(S0L)
-        ret
-
-
-J_RESET:
-        inc YL
-        inc YL
-        ldi R21, 0x0C
-        cpse YL, R21
-        ret
-        LDI YL, low(0x0220)   ; reset Y pointer to start of L-array
-        ret
+    lcd
+	KEY_EXP
+	ENCRYPTION
+	lcd
+	DECRYPTION
+	lcd
+	end_f:  RJMP  end_f
